f145e3632ebfa1b77b7ff51f7f3ade187740b802
allan welerson <allanwelerson@gmail.com>
2021-03-15 20:43:20 -0300

    Adicionando arquivo de Layout e Controller de administração de usuários

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -26,4 +27,13 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
+
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function() {
+        Route::resource('users', UserController::class);
+});
+
