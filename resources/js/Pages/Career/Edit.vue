724e4590fea631bb7768efaf4ff9aa2f36692dba
allan welerson <allanwelerson@gmail.com>
2021-04-24 21:35:00 -0300

    Implementados cards

--- resources/js/Pages/Career/Edit.vue
+++ resources/js/Pages/Career/Edit.vue
@@ -1,7 +1,19 @@
 <template lang="">
-    <Layout :title="'Atualização do Curso'" :routeButton="'careers.index'" :textButton="'Lista'">
-        <Career-form :form="form" @salvar="salvar"></Career-form>
-    </Layout>
+    <div class="grid-cols-12">
+        <layout>
+            <div class="flex justify-between">
+                    <div class="text-white text-4xl">
+                        Cursos
+                    </div>
+                    <inertia-link
+                        class="text-white text-center text-xl capitalize rounded-md bg-green-500 hover:bg-green-600 pl-2 pr-3 py-3 font-bold"
+                        href="/cursos"
+                        ><i class="fas fa-tv opacity-75 mr-2 text-sm"></i>
+                        Lista</inertia-link>
+            </div>
+            <Career-form :form="form" @salvar="salvar"></Career-form>
+        </layout>
+    </div>
 </template>
 
 <script>
