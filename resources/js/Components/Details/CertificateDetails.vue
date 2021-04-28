58575a56a79837e8d7dcda5f51106f5def6a1ac3
mateus.barros1 <mateus.barros@dpu.def.br>
2021-04-28 20:18:30 -0300

    Estilização inputs

--- resources/js/Components/Details/CertificateDetails.vue
+++ resources/js/Components/Details/CertificateDetails.vue
@@ -1,45 +1,62 @@
 <template lang="">
-    <div class="grid grid-cols-3 2xl:grid-cols-7 gap-2 border border-gray-200 rounded">
-        <div class="p-2 col-span-3 2xl:col-start-3 p-8 ">
-
-            <div class="w-full flex flex-col justify-center text-center">
-                <div class="mb-4"><b class="w-full block py-2 px-3 border rounded-md border-gray-300">Nome:</b> <p class="mt-4">{{certificate.user.name}}</p></div>
-                <div class="mb-4"><b class="w-full block py-2 px-3 border rounded-md border-gray-300">Titulo: </b> <p class="mt-4">{{certificate.title}}</p></div>
-                <div class="mb-4"><b class="w-full block py-2 px-3 border rounded-md border-gray-300">Situação:</b> <p class="mt-4">{{certificate.type_situation.name}}</p></div>
-                <div class="mb-4"><b class="w-full block py-2 px-3 border rounded-md border-gray-300">Eixo:</b> <p class="mt-4">{{certificate.axle?.name}}</p></div>
-                <div class="mb-4"><b class="w-full block py-2 px-3 border rounded-md border-gray-300">Descrição:</b> <p class="mt-4">{{certificate.description}}</p></div>
+    <!--div class="grid grid-cols-3 2xl:grid-cols-7 gap-2 border border-gray-200 rounded"-->
+        <!-- Card Details -->
+        <center>
+            <div class="object-none object-center">
+                <div class="max-w-xl bg-white shadow-lg rounded-lg overflow-hidden my-4 justify-center text-center">
+                    <div class="flex items-center px-6 py-3 bg-green-uniceplac justify-center text-center">
+                        
+                        <h1 class="mx-3 text-white font-semibold text-lg ">{{certificate.title}}</h1>
+                    </div>
+                    
+                    <div class="py-4 px-6">
+                        <h1 class="text-2xl font-semibold text-gray-800">{{certificate.user.name}}</h1>
+                        
+                        <p class="py-2 text-lg text-gray-700">Descrição: <br><span>{{certificate.description}}</span></p>
+                        
+                        <div class="flex items-center mt-4 text-gray-700">
+                            
+                            <h1 class="px-2 text-sm">Situação: {{certificate.type_situation.name}}</h1>
+                        </div>
+                        <div class="flex items-center mt-4 text-gray-700">
+                            
+                            <h1 class="px-2 text-sm">Eixo: {{certificate.axle?.name}}</h1>
+                        </div>
+                            <div class="flex flex-row justify-end text-center mb-4">
+                                <a  :href="getRouteDownload(certificate.id)"
+                                    target="_blank"
+                                    class="text-white ml-2 text-center capitalize rounded-md bg-green-uniceplac hover:bg-orange-uniceplac pl-2 pr-3 py-2 font-bold"
+                                >Visualizar Arquivo</a>
+                            </div>
+                        
+                    </div>
+                </div>
+                
             </div>
 
-            <div class="flex flex-row justify-end text-center mb-4">
-                <a  :href="getRouteDownload(certificate.id)"
-                    target="_blank"
-                    class="text-white ml-2 text-center capitalize rounded-md bg-green-500 hover:bg-green-600 pl-2 pr-3 py-2 font-bold"
-                >Visualizar Arquivo</a>
-            </div>
-
-        </div>
-    </div>
+        </center>
+    <!--/div-->
 </template>
 
 <script>
-
 export default {
-    props: {
-        certificate: Object,
+  props: {
+    certificate: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    getRouteDownload(id) {
+      if (
+        this.$page.props.hasHole.admin ||
+        this.$page.props.hasHole.validator
+      ) {
+        return this.route("validator.certificates.download", id);
+      } else {
+        return this.route("certificates.download", id);
+      }
     },
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-        getRouteDownload(id) {
-            if (this.$page.props.hasHole.admin || this.$page.props.hasHole.validator) {
-                return this.route('validator.certificates.download', id);
-            } else {
-                return this.route('certificates.download', id);
-            }
-        }
-    },
-}
+  },
+};
 </script>
--- resources/js/Components/Forms/CertificateForm.vue
+++ resources/js/Components/Forms/CertificateForm.vue
@@ -5,23 +5,29 @@
                 <div class="grid grid-cols-4 gap-4 mb-4">
                     <div class="col-span-4">
                         <label for="certificate_title">Titulo</label>
-                        <input type="text" class="w-full" id="certificate_title" maxlength="100" required v-model="form.title" autofocus/>
+                            
+                        <input type="text" class="w-full focus:ring-orange-uniceplac focus:border-orange-uniceplac block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md shadow-md" id="certificate_title" maxlength="100" required v-model="form.title" autofocus/>
                     </div>
                     <div class="col-span-4">
                         <label for="axle_description">Descrição</label>
                         <textarea   type="text"
-                                    class="w-full"
+                                    class="w-full focus:ring-orange-uniceplac focus:border-orange-uniceplac block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md shadow-md"
                                     id="axle_description"
                                     rows="5"
                                     required
                                     v-model="form.description"></textarea>
                     </div>
+                    
                     <div class="col-span-4">
-                        <input type="file" @input="form.archive = $event.target.files[0]" />
-                        <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-                            {{ form.progress.percentage }}%
-                        </progress>
+                        
+                        <input class="w-96 flex flex-col items-center px-4 py-6 bg-grey-0 rounded-lg shadow-lg cursor-pointer hover:bg-gray-100" type="file" @input="form.archive = $event.target.files[0]"/>
+                        
+                            
+                        <!--progress class="" v-if="form.progress" :value="form.progress.percentage" max="100">
+                                {{ form.progress.percentage }}%
+                        </progress-->
                     </div>
+                    
                 </div>
 
                 <div class="mt-4 w-full flex justify-end">
@@ -35,20 +41,17 @@
 </template>
 
 <script>
-
 export default {
-    props: {
-        form: Object,
+  props: {
+    form: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    salvar() {
+      this.$emit("salvar");
     },
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-        salvar() {
-            this.$emit('salvar')
-        }
-    },
-}
+  },
+};
 </script>
