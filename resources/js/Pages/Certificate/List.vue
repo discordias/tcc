eb91adaccf3862ccd4740356440398c2e29edef3
Guilherme Rodrigues <guilhermer271@gmail.com>
2021-05-02 21:34:54 -0300

    Modificação botão de cadastro

--- resources/js/Pages/Certificate/List.vue
+++ resources/js/Pages/Certificate/List.vue
@@ -1,5 +1,5 @@
 <template lang="">
-    <Layout :title="`Certificados - Situação ${currentTypeSituation.name}`" :routeButton="'certificates.create'" :textButton="'Cadastro'">
+    <Layout :title="`Certificados - Situação: ${currentTypeSituation.name}`" :routeButton="routeButton" :textButton="textButton" v-if="certificates.data.length > 0">
         <!-- component -->
         <div class="w-full overflow-x-scroll">
             <div class="bg-white shadow-md rounded my-6">
@@ -41,7 +41,6 @@
                                     ><span class="text-sm">Editar</span></inertia-link>
                             </td>
                         </tr>
-
                     </tbody>
                 </table>
                 <div class="px-6 pb-4 bg-white">
@@ -49,7 +48,22 @@
                 </div>
             </div>
         </div>
+
     </Layout>
+
+    <Layout :title="`Certificados - Situação: ${currentTypeSituation.name}`" v-if="certificates.data.length == 0"> 
+                <div class="bg-yellow-100 border border-yellow-200 rounded my-6 ">
+            <p class="py-3 px-10"> Não foi encontrado nenhum registro de documento em nosso banco de dados, caso queira cadastrar um documento por favor clique no botão "Cadastro" </p>
+        </div>
+        <div class="text-center py-3">
+                <inertia-link
+                class="text-white text-center text-xl capitalize rounded-md bg-green-500 hover:bg-green-600 pl-2 pr-3 py-3 font-bold"
+                :href="this.route('certificates.create')"
+                ><span fas fa-tv opacity-75 mr-2 text-sm>Cadastro</span>
+                </inertia-link>
+                </div>
+    </Layout>
+    
 </template>
 
 <script>
@@ -64,6 +78,8 @@ export default {
   },
   data() {
     return {
+
+         routeButton: "certificates.create", textButton: "Cadastro"
 
     };
   },
